792ffbb6ce9e8ab5b3cd9a2fd7f2b82ac0be502d
JoseMRegalado <mjregalado2@utpl.edu.ec>
2022-05-10 00:23:53 -0500

    xdd

--- problema3/Problema3/src/problema3/Ejecutor3.java
+++ problema3/Problema3/src/problema3/Ejecutor3.java
@@ -4,6 +4,12 @@ package problema3;
 
 public class Ejecutor3 {
     public static void main(String[] args) {
-        
+        Automotor au = new Automotor("1109567832","Mazda");
+        Automotor au2 = new Automotor("1105647321","Jeep",2019);
+        au.establecerAnio(2015);
+        au.calcularValorMatricula();
+        System.out.printf("%s\n", au);
+        au2.calcularValorMatricula();
+        System.out.printf("%s\n", au2);
     }
 }
